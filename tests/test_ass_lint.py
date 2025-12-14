70643c0a8bed99a75e4530fc2c25552411a0f186
banteg <4562643+banteg@users.noreply.github.com>
2025-12-14 23:44:25 +0400

    test: switch to pytest

--- tests/test_ass_lint.py
+++ tests/test_ass_lint.py
@@ -1,5 +1,3 @@
-import unittest
-
 from c2_overlay.ass_lint import lint_ass_text, parse_ass_time
 
 
@@ -15,45 +13,48 @@ Format: Layer, Start, End, Style, Name, MarginL, MarginR, MarginV, Effect, Text
 """
 
 
-class TestAssLint(unittest.TestCase):
-    def test_parse_ass_time(self) -> None:
-        t0 = parse_ass_time("0:00:00.00")
-        self.assertIsNotNone(t0)
-        self.assertAlmostEqual(t0, 0.0)
+def test_parse_ass_time() -> None:
+    t0 = parse_ass_time("0:00:00.00")
+    assert t0 is not None
+    assert t0 == 0.0
 
-        t1 = parse_ass_time("1:02:03.04")
-        self.assertIsNotNone(t1)
-        self.assertAlmostEqual(t1, 3723.04)
-        self.assertIsNone(parse_ass_time("nope"))
+    t1 = parse_ass_time("1:02:03.04")
+    assert t1 is not None
+    assert t1 == 3723.04
 
-    def test_detect_overlap(self) -> None:
-        ass = (
-            ASS_HEADER
-            + "Dialogue: 6,0:00:00.00,0:00:02.00,Distance,,0,0,0,,{\\pos(10,10)}100\n"
-            + "Dialogue: 9,0:00:01.00,0:00:03.00,Distance,,0,0,0,,{\\pos(10,10)}101\n"
-        )
-        issues = lint_ass_text(ass)
-        self.assertTrue(any(i.code == "ASS020" and i.severity == "error" for i in issues))
-
-    def test_no_overlap_for_touching_boundaries(self) -> None:
-        ass = (
-            ASS_HEADER
-            + "Dialogue: 9,0:00:00.00,0:00:01.00,Distance,,0,0,0,,{\\pos(10,10)}0\n"
-            + "Dialogue: 9,0:00:01.00,0:00:02.00,Distance,,0,0,0,,{\\pos(10,10)}1\n"
-        )
-        issues = lint_ass_text(ass)
-        self.assertFalse(any(i.code == "ASS020" for i in issues))
-
-    def test_detect_bad_duration(self) -> None:
-        ass = ASS_HEADER + "Dialogue: 9,0:00:01.00,0:00:01.00,Distance,,0,0,0,,{\\pos(10,10)}1\n"
-        issues = lint_ass_text(ass)
-        self.assertTrue(any(i.code == "ASS010" and i.severity == "error" for i in issues))
-
-    def test_warn_on_missing_pos(self) -> None:
-        ass = ASS_HEADER + "Dialogue: 9,0:00:00.00,0:00:01.00,Distance,,0,0,0,,100\n"
-        issues = lint_ass_text(ass)
-        self.assertTrue(any(i.code == "ASS012" and i.severity == "warn" for i in issues))
+    assert parse_ass_time("nope") is None
 
 
-if __name__ == "__main__":
-    unittest.main()
+def test_detect_overlap() -> None:
+    ass = (
+        ASS_HEADER
+        + "Dialogue: 6,0:00:00.00,0:00:02.00,Distance,,0,0,0,,{\\pos(10,10)}100\n"
+        + "Dialogue: 9,0:00:01.00,0:00:03.00,Distance,,0,0,0,,{\\pos(10,10)}101\n"
+    )
+    issues = lint_ass_text(ass)
+    assert any(i.code == "ASS020" and i.severity == "error" for i in issues)
+
+
+def test_no_overlap_for_touching_boundaries() -> None:
+    ass = (
+        ASS_HEADER
+        + "Dialogue: 9,0:00:00.00,0:00:01.00,Distance,,0,0,0,,{\\pos(10,10)}0\n"
+        + "Dialogue: 9,0:00:01.00,0:00:02.00,Distance,,0,0,0,,{\\pos(10,10)}1\n"
+    )
+    issues = lint_ass_text(ass)
+    assert not any(i.code == "ASS020" for i in issues)
+
+
+def test_detect_bad_duration() -> None:
+    ass = (
+        ASS_HEADER
+        + "Dialogue: 9,0:00:01.00,0:00:01.00,Distance,,0,0,0,,{\\pos(10,10)}1\n"
+    )
+    issues = lint_ass_text(ass)
+    assert any(i.code == "ASS010" and i.severity == "error" for i in issues)
+
+
+def test_warn_on_missing_pos() -> None:
+    ass = ASS_HEADER + "Dialogue: 9,0:00:00.00,0:00:01.00,Distance,,0,0,0,,100\n"
+    issues = lint_ass_text(ass)
+    assert any(i.code == "ASS012" and i.severity == "warn" for i in issues)
